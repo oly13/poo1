0f9c9baae4797be1e4890fd33cbef7077e49509a
pablogarciabenayas <pablogarciabenayas@gmail.com>
2015-12-29 18:12:36 +0100

    Prueba para github

---
+++ src/elecciones/pruebaGIT.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elecciones;
+
+/**
+ *
+ * @author pablo
+ */
+public class pruebaGIT {
+    private String prueba = "esto es una prueba en GIT";
+    
+}
